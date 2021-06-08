2f1f3767a67703c4665c2070c904b3e731bb5bee
suryaravikumar-pixer <suryaravikumar@gmail.com>
2021-06-08 08:35:36 +0530

    fixtures, setup and teardown methods added

--- pytest_dir4/test_student.py
+++ pytest_dir4/test_student.py
@@ -1,27 +1,29 @@
 from student import StudentDB
 import pytest
 
-
-db = None
-"""setup_module is initiate's the dabase connection for each test module"""
-def setup_module(module):
+"""with this fixture we can return db to the test cases but it calls setupmodule for each test, we want setupmodule begining of the test """
+# @pytest.fixture 
+"""yeah, this worked well interms of our requirement"""
+@pytest.fixture(scope='module')
+def db():
     print('-----------setupmodule---------------')
-    global db
     db = StudentDB()
     db.connect('data.json')
+    yield db #return replaced by yield, at end of the test close connection will executed 
 
-"""teardown_module is closes the db connection """
-def teardown_module(module):
+    """teardown_module is closes the db connection """
     print('-----------teardown ---------------')
     db.close()
+    
 
-def test_jhon_data():
+
+def test_jhon_data(db):
     Jhon_data = db.get_data('Jhon')
     assert Jhon_data['id'] == 1
     assert Jhon_data['name'] == 'Jhon'
     assert Jhon_data['result'] == 'pass'
 
-def test_mark_data():
+def test_mark_data(db):
     Mark_data = db.get_data('Mark')
     assert Mark_data['id'] == 2
     assert Mark_data['name'] == 'Mark'
